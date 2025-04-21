567852b208ae986d96b3d9c5fea5169c78d0308e
EgorUU <egorsai@gmail.com>
2025-04-21 17:44:38 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 const repoName = '/Chat'
 const nextConfig: NextConfig = {
     output: 'export',
-    assetPrefix: repoName,
-    basePath: repoName,
 };
 
 export default nextConfig;
